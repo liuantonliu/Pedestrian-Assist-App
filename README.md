ea6aa3453e1f3faa15daf77a222aec22c91b46d1
Anton Liu <52012670+liuantonliu@users.noreply.github.com>
2020-08-10 23:17:13 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Crossy-Road
+Pedestrian fatalities were at a 25 year high in 2017. One of the main causes? Lack of awareness. Our solution notifies the user when it's safe to cross the road to decrease pedestrian fatalities.
+
+Authors: The Rubber Duckies
+
+Framework credit goes to Sri Raghu Malireddi / [@r4ghu](https://sriraghu.com)
---
+++ WORKING MODEL 3.0/iOS-CoreML-Inceptionv3/PreviewView.swift
@@ -0,0 +1,29 @@
+//
+//  PreviewView.swift
+//  HelloCamera
+//
+//  Created by TheRubberDuckies
+//  23/06/19
+
+import UIKit
+import AVFoundation
+
+class PreviewView: UIView {
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+}
+
